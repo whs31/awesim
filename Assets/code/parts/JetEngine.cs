ec0c7b93a23200e93c400a124fe6748cc47bb0bd
Рязанцев Дмитрий <75864292+whs31@users.noreply.github.com>
2023-02-27 18:40:08 +0300

    Engine thrust and debug draw layer

--- Assets/code/parts/JetEngine.cs
+++ Assets/code/parts/JetEngine.cs
@@ -8,14 +8,21 @@ public class JetEngine : MonoBehaviour
     [SerializeField] private float m_intensityMultiplier;
     [SerializeField] private Material m_material;
 
+    [SerializeField] private GameObject m_physicsObject;
 
-    void Start()
-    {
-        
-    }
+    [Space(10)]
+    [Header("Engine Properties")]
+    [Tooltip("In kilogram-force")] [SerializeField] private float m_thrust = 4100;
 
     void Update()
     {
         m_material.SetVector("_EmissionColor", m_emissionColorValue * m_intensityMultiplier * PlayerInput.Throttle);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 forwardForce = (PlayerInput.Throttle * m_thrust) * (-m_physicsObject.transform.forward);
+        m_physicsObject.GetComponent<Rigidbody>().AddForce(forwardForce);
+        DrawArrow.DebugColor(m_physicsObject.transform.position, forwardForce / 500, Color.red);
     }
 }
